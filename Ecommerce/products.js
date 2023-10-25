a5317ef99df6094f5fc94497d9b84056c603be21
danisa-morales <moralesc.danisa@gmail.com>
2023-10-25 15:36:08 -0300

    Archivos index.html, products.js, main.js, style.css y carpeta img

---
+++ Ecommerce/products.js
@@ -0,0 +1,74 @@
+export const products = [
+    {
+        id: 1,
+        name: "Galletitas rellenas",
+        price: 600,
+        image: "/img/galletas.jpg",
+        description: "producto sin tacc",
+        category : 'tacc',
+        stock : 10
+    },
+    {
+        id: 2,
+        name: "Alfajor",
+        price: 600,
+        image: "/img/alfajor.jpeg",
+        description: "producto vegano",
+        category : 'vegano',
+        stock : 10
+    },
+    {
+        id: 3,
+        name: "Masitas",
+        price: 600,
+        image: "/img/masitas.jpg",
+        description: "producto vegetariano",
+        category : 'vegetariano',
+        stock : 10
+    },
+    {
+        id: 4,
+        name: "Pan",
+        price: 600,
+        image: "/img/pan.webp",
+        description: "producto free cruelty",
+        category : 'free-cruelty',
+        stock : 10
+    },
+    {
+        id: 5,
+        name: "Polenta",
+        price: 600,
+        image: "/img/polenta.jpeg",
+        description: "producto sin tacc",
+        category : 'tacc',
+        stock : 10
+    },
+    {
+        id: 6,
+        name: "Queso",
+        price: 600,
+        image: "/img/queso.webp",
+        description: "producto sin tacc",
+        category : 'tacc',
+        stock : 10
+    },
+    {
+        id: 7,
+        name: "Granola",
+        price: 600,
+        image: "/img/granola.webp",
+        description: "producto sin tacc",
+        category : 'tacc',
+        stock : 10
+    },
+    {
+        id: 8,
+        name: "Chips",
+        price: 600,
+        image: "/img/chips.png",
+        description: "producto sin tacc",
+        category : 'tacc',
+        stock : 10
+    }
+]
