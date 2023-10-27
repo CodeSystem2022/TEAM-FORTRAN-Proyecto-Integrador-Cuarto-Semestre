db23b3ea8b3307f7cf732bbaf06290a40793f128
Facundo Pereyra <facundo070598@gmail.com>
2023-10-27 15:20:09 -0300

    Agregar productos seccion "Productos naturales"
    *Agrego productos al área de "Productos naturales" con 20 nuevos productos y sus imágenes.

--- Ecommerce/products.js
+++ Ecommerce/products.js
@@ -188,5 +188,186 @@ export const products = [
         category : 'Keto',
         stock : 10
     },
+    {
+        id: 22,
+        name: "Almendras",
+        price: 4300,
+        image: "/img/Almendras.jpg",
+        description: "Productos Naturales",
+        category : 'Naturales',
+        stock : 8
+    },
+    {
+        id: 23,
+        name: "Banana Deshidratada",
+        price: 2750,
+        image: "/img/BananaDeshidratada.jpg",
+        description: "Productos Naturales",
+        category : 'Naturales',
+        stock : 5
+    },
+    {
+        id: 24,
+        name: "Aanana en Rodajas",
+        price: 3280,
+        image: "/img/AananaEnRodajas.jpg",
+        description: "Productos Naturales",
+        category : 'Naturales',
+        stock : 6.2
+    },
+    {
+        id: 25,
+        name: "Avellanas",
+        price: 3700,
+        image: "/img/Avellanas.jpg",
+        description: "Productos Naturales",
+        category : 'Naturales',
+        stock : 15.4
+    },
+    {
+        id: 26,
+        name: "Castañas de Caju",
+        price: 3750,
+        image: "/img/CastañasDeCaju.jpg",
+        description: "Productos Naturales",
+        category : 'Naturales',
+        stock : 2.3
+    },
+    {
+        id: 27,
+        name: "Mix Frutos Secos",
+        price: 1450,
+        image: "/img/MixFrutosSecos.jpg",
+        description: "Productos Naturales",
+        category : 'Naturales',
+        stock : 24
+    },
+    {
+        id: 28,
+        name: "Nuez Mariposa",
+        price: 2880,
+        image: "/img/NuezMariposa.jpg",
+        description: "Productos Naturales",
+        category : 'Naturales',
+        stock : 7.6
+    },
+    {
+        id: 29,
+        name: "Pasas de uva",
+        price: 920,
+        image: "/img/PasasDeUva.jpg",
+        description: "Productos Naturales",
+        category : 'Naturales',
+        stock : 12.6
+    },
+    {
+        id: 30,
+        name: "Arandanos Deshidratados",
+        price: 2250,
+        image: "/img/ArandanosDeshidratados.jpg",
+        description: "Productos Naturales",
+        category : 'Naturales',
+        stock : 9.7
+    },
+    {
+        id: 31,
+        name: "Ciruelas Sin Carozo",
+        price: 1350,
+        image: "/img/CiruelasSinCarozo.jpg",
+        description: "Productos Naturales",
+        category : 'Naturales',
+        stock : 4.7
+    },
+    {
+        id: 32,
+        name: "Dátiles Mdejol",
+        price: 3000,
+        image: "/img/DátilesMdejol.jpg",
+        description: "Productos Naturales",
+        category : 'Naturales',
+        stock : 3.1
+    },
+    {
+        id: 33,
+        name: "Durazno Deshidratados",
+        price: 1500,
+        image: "/img/DuraznoDeshidratados.jpg",
+        description: "Productos Naturales",
+        category : 'Naturales',
+        stock : 1.5
+    },
+    {
+        id: 34,
+        name: "Higo Negro",
+        price: 1700,
+        image: "/img/HigoNegro.jpg",
+        description: "Productos Naturales",
+        category : 'Naturales',
+        stock : 5.7
+    },
+    {
+        id: 35,
+        name: "Higo Turco",
+        price: 3875,
+        image: "/img/HigoTurco.jpg",
+        description: "Productos Naturales",
+        category : 'Naturales',
+        stock : 4.3
+    },
+    {
+        id: 36,
+        name: "Kiwi Deshidratado",
+        price: 2190,
+        image: "/img/KiwiDeshidratado.jpg",
+        description: "Productos Naturales",
+        category : 'Naturales',
+        stock : 2.6
+    },
+    {
+        id: 37,
+        name: "Mango Deshidratado",
+        price: 3525,
+        image: "/img/MangoDeshidratado.jpg",
+        description: "Productos Naturales",
+        category : 'Naturales',
+        stock : 8.8
+    },
+    {
+        id: 38,
+        name: "Melon Deshidratado",
+        price: 1985,
+        image: "/img/MelonDeshidratado.jpg",
+        description: "Productos Naturales",
+        category : 'Naturales',
+        stock : 10.6
+    },
+    {
+        id: 39,
+        name: "Pera Deshidratada",
+        price: 1463,
+        image: "/img/PeraDeshidratada.jpg",
+        description: "Productos Naturales",
+        category : 'Naturales',
+        stock : 12.5
+    },
+    {
+        id: 40,
+        name: "Tiritas De Naranja",
+        price: 2100,
+        image: "/img/TiritasDeNaranja.jpg",
+        description: "Productos Naturales",
+        category : 'Naturales',
+        stock : 7.1
+    },
+    {
+        id: 41,
+        name: "Tomates Secos",
+        price: 1900,
+        image: "/img/TomatesSecos.jpg",
+        description: "Productos Naturales",
+        category : 'Naturales',
+        stock : 14.2
+    }
+
   
 ]
